845e367000768c97407349b7bfb685f625d4ab2a
Rayan Khyare <rayan.khyare@student.ehb.be>
2024-05-13 19:47:55 +0200

    working on header

--- flux/src/app/components/Header/Header.tsx
+++ flux/src/app/components/Header/Header.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import './header.scss';
+
 import Image from 'next/image'
+
+import { Input } from '@headlessui/react'
+import Link from 'next/link'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+
 const logo = "/images/flux_logo.svg";
+const footballIcon = "/images/football_icon.svg";
 
 const Header: React.FC = () => {
     return (
         <header>
-            <Image width={200} height={200} alt="logo" src={logo} />
+
+            <Link href="/">
+            <Image width={100} height={100} alt="logo" src={logo} />
+            </Link>
+
+            <Input name="full_name" type="text" />
+
             <nav>
-                <ul>
-                    <li>
-                        <a href="/">Home</a>
-                    </li>
-                    <li>
-                        <a href="/about">About</a>
-                    </li>
-                    <li>
-                        <a href="/contact">Contact</a>
-                    </li>
-                </ul>
+                    <Menu>
+                        <MenuButton><Image width={40} height={40} alt="logo" src={footballIcon} /></MenuButton>
+                        <MenuItems anchor="bottom">
+                            <MenuItem>
+                            <a className="block data-[focus]:bg-blue-100" href="/settings">
+                                Settings
+                            </a>
+                            </MenuItem>
+                            <MenuItem>
+                            <a className="block data-[focus]:bg-blue-100" href="/support">
+                                Support
+                            </a>
+                            </MenuItem>
+                            <MenuItem>
+                            <a className="block data-[focus]:bg-blue-100" href="/license">
+                                License
+                            </a>
+                            </MenuItem>
+                        </MenuItems>
+                    </Menu>
             </nav>
         </header>
     );
