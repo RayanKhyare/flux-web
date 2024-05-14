5d0aced7fead5c884c53ecf12ce38b3e86e3fbd1
Rayan Khyare <rayan.khyare@2mprove.be>
2024-05-14 14:30:40 +0200

    add: done with header

--- flux/src/app/components/Header/Header.tsx
+++ flux/src/app/components/Header/Header.tsx
@@ -1,47 +1,75 @@
-import React from 'react';
+"use client";
+
+import React , {useState} from 'react';
 import './header.scss';
 
 import Image from 'next/image'
 
 import { Input } from '@headlessui/react'
 import Link from 'next/link'
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Menu, MenuButton, MenuItem, MenuItems , 
+        Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
 
 const logo = "/images/flux_logo.svg";
 const footballIcon = "/images/football_icon.svg";
+const newsIcon = "/images/news_icon.svg";
+const bellIcon = "/images/bell_icon.svg";
 
 const Header: React.FC = () => {
+    const [isLogged, setIsLoggen] = useState(false);
     return (
         <header>
 
             <Link href="/">
-            <Image width={100} height={100} alt="logo" src={logo} />
+            <Image width={80} height={80} alt="logo" src={logo} />
             </Link>
 
-            <Input name="full_name" type="text" />
+            <input
+                className="rounded-full p-2"
+                placeholder="Search"
+            />
 
             <nav>
                     <Menu>
                         <MenuButton><Image width={40} height={40} alt="logo" src={footballIcon} /></MenuButton>
-                        <MenuItems anchor="bottom">
+                        {/* <MenuItems anchor="bottom">
                             <MenuItem>
                             <a className="block data-[focus]:bg-blue-100" href="/settings">
                                 Settings
                             </a>
                             </MenuItem>
-                            <MenuItem>
-                            <a className="block data-[focus]:bg-blue-100" href="/support">
-                                Support
-                            </a>
-                            </MenuItem>
-                            <MenuItem>
-                            <a className="block data-[focus]:bg-blue-100" href="/license">
-                                License
-                            </a>
-                            </MenuItem>
-                        </MenuItems>
+                        </MenuItems> */}
                     </Menu>
+
+                    <Image width={40} height={40} alt="logo" src={newsIcon} />
+                    <Image width={40} height={40} alt="logo" src={bellIcon} />
+
+                    {isLogged && 
+                        <Popover className="relative">
+                            <PopoverButton>Solutions</PopoverButton>
+                            <PopoverPanel anchor="bottom" className="flex flex-col">
+                                <a href="/analytics">Analytics</a>
+                                <a href="/engagement">Engagement</a>
+                                <a href="/security">Security</a>
+                                <a href="/integrations">Integrations</a>
+                            </PopoverPanel>
+                        </Popover>
+                    }
+
+                    {!isLogged && 
+                        <Link href="/login">
+                            <h3 className="">Log in</h3>
+                        </Link>
+                    }
+
+                    {!isLogged && 
+                    <Link href="/signup">
+                        <button className="">Join us</button>
+                    </Link>
+                    }
             </nav>
+
+
         </header>
     );
 };
